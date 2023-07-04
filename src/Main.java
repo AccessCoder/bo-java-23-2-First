538d421e699963bb15edb4b0206df2768259f45b
Dominic <andro5@web.de>
2023-07-04 10:03:05 +0200

    add int values and if/else

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!" + (1 + 5));
 
+        int a = 2;
+        int b = 4;
+        int c = 6;
+
+        int summe = b+c;
+        System.out.println(summe);
+
+        if (a < 10 && b == 4){
+            System.out.println("la");
+        } else {
+            System.out.println("bla");
+        }
+    }
     }
 }
